51d4483f810e11022c22c7262ed034db8b47dc7c
Olej <music.olejniczak@wp.pl>
2023-03-29 01:42:29 +0200

    good progress

--- btree.py
+++ btree.py
@@ -1,7 +1,5 @@
-from binarytree import build
-
-
 # Klasa reprezentujaca pojedynczy wezel drzewa
+# Każdy Korzeń reprezentuje drzewo 
 class Node:
     def __init__(self, value=None):
         # Wartosc przechowywana w wezle
@@ -34,6 +32,7 @@ class Node:
                     self.right.add(value, height)
         else:
             self.value = value
+            self.height = 0
 
     def getHeight(self, heightList = []):
         if self.left is None and self.right is None:
@@ -53,6 +52,127 @@ class Node:
             total += self.right.getNumNodes()
         return total + 1
 
+    def MakeAvl(self, array):
+        arra = array[:]
+        arra.sort()
+        arr = binarySplit(arra)
+        for x in arr:
+            self.add(x)
+        return self
+    
+    def MakeBst(self, array):
+        for x in array:
+            self.add(x)
+        return self
+    
+    def MakeDegen(self, array):
+        arr = array[:]
+        arr.sort()
+        for x in arr:
+            self.add(x)
+        return self
+    
+    def getMin(self):
+        if self.left is None:
+            global minVal, minValHeight
+            minVal = self.value
+            minValHeight = self.height
+        else:
+            print(f"{self.value} - {self.height}")
+            self.left.getMin()
+        return f"{minVal} - {minValHeight}"
+    
+    def getMax(self):
+        if self.right is None:
+            global maxVal, maxValHeight
+            maxVal = self.value
+            maxValHeight = self.height
+        else:
+            print(f"{self.value} - {self.height}")
+            self.right.getMax()
+        return f"{maxVal} - {maxValHeight}"
+    
+    def heightElements(self):
+        totalHeight = self.getHeight()
+        heightElements = {}
+        for i in range(totalHeight+1):
+            heightElements[f"height-{i}"] = []
+        return heightElements
+    
+    #idzie in-order przez elementy więc zapisuje jakie elementy
+    #będą od lewej do prawej. Idealnie, żeby wyprincić B)
+    def assignHeight(self, hDict):
+        hDict[f"height-{self.height}"] += [self.value]
+        if self.left:
+            self.left.assignHeight(hDict)
+        if self.right:
+            self.right.assignHeight(hDict)
+        return hDict
+    
+    # fills tree with 0-nodes so it can be printed prettily #
+    def treeFiller(self):
+        totalHeight = self.getHeight()
+        if self.left is None and self.height < totalHeight:
+            self.left = Node(0)
+            self.left.height = self.height + 1
+        elif self.height < totalHeight:
+            self.left.treeFiller()
+        if self.right is None and self.height < totalHeight:
+            self.right = Node(0)
+            self.right.height = self.height + 1
+        elif self.height < totalHeight:
+            self.right.treeFiller()
+
+
+
+
+    # only run for AVL TREES!!!! #
+    def prettyPrint(self):
+        totalHeight = self.getHeight()
+        for x in range(totalHeight):
+            self.treeFiller()
+        hDictEmpty = self.heightElements()
+        hDict = self.assignHeight(hDictEmpty)
+        width = 2 ** (totalHeight+2)
+        level = 1
+        for k, v in hDict.items():
+            widthT = int(width/level)
+            level *= 2
+            if k == f"height-{totalHeight+1}":
+                return "Here's your tree!"
+            else:
+                print(f"{k}", end="\t")
+                print("|", end="")
+                for val in v:
+                    print(f"{str(val):^{widthT}}", end="")
+                print("|")
+
+    def degPrint(self):
+        totalHeight = self.getHeight()
+        hDictEmpty = self.heightElements()
+        hDict = self.assignHeight(hDictEmpty)
+        level = 5
+        for k, v in hDict.items():
+            level += 1
+            print(f"{k}|", end="\t")
+            print(f"{str(v[-1]):>{level}}")
+            
+
+                
+
+
+#Makes an IN-ORDER list
+def binarySplit(array, binList=[]):
+    arr = array[:]
+    if len(arr) < 1:
+        return []
+    middle_index = len(arr) // 2
+    middle_element = arr.pop(middle_index)
+    left_half = arr[:middle_index]
+    right_half = arr[middle_index:]
+    binList = [middle_element] + binarySplit(left_half, binList) + binarySplit(right_half, binList)
+    return binList
+
 
 # Rekurencyjne przeszukiwanie drzewa
 #IN-ORDER
@@ -64,57 +184,33 @@ def search(node):
         search(node.right)
 
 
-test = [1,2,3,4,5,6,7,8,9,10,11,12]
 
-#Makes an IN-ORDER list
-def binarySplit(arr):
-    if len(arr) < 1:
-        return []
-    middle_index = len(arr) // 2
-    middle_element = arr.pop(middle_index)
-    left_half = arr[:middle_index]
-    right_half = arr[middle_index:]
-    return binarySplit(right_half) + binarySplit(left_half) + [middle_element]
-
-def CreateAVLTree(arr):
-    arr.sort()
-    binList = binarySplit(arr)
-    binList.reverse()
-    root = Node()
-    for n in binList:
-        root.add(n)
-    return root
-
-def CreateTree(arr):
-    root = Node()
-    for n in arr:
-        root.add(n)
-    return root
-    
-def printTree(root):
     
 
-testList = [1,2,3,4,5,6,7,8,9,10,11,12]
-testList2 = [x for x in range(40)]
+testList = [x for x in range(1, 41)]
+testList2 = [6, 4, 3, 7, 2, 10, 5, 9, 10]
+degTest = [x for x in range(1, 16)]
 
-avl = CreateAVLTree(testList)
+# --- TEST ZONE --- #
 
+root1 = Node()
+root2 = Node()
 
+avl = root1.MakeAvl(testList)
+bst = root2.MakeBst(degTest)
 
+# print(avl)
+# print(avl.getHeight())
+# print(avl.getNumNodes())
+# print("- "*10)
+# print(bst)
+# print(bst.getHeight())
+# print(bst.getNumNodes())
 
+# print(avl.getMin())
+# print(minVal)
+# print(bst.getMin())
+# print(minVal)
 
-
-
-
-
-
-# # Korzen drzewa
-# root = Node(7)
-# # Dodajemy dzieci "recznie"
-# root.left = Node(4)
-# root.right = Node(9)
-# root.left.right = Node(5)
-
-# # Przeszukujemy drzewo w kolejnosci in-order
-# search(root)
-
+print(avl.getMin())
+print(avl.getMax())
--- test.py
+++ test.py
@@ -1,1 +1,42 @@
 
+# print(f"    [2] \n   /  \ \n  [1] [3]", end="")
+# print("asdasda \n")
+
+# print(" "*4, end="")
+# print("[2]", end="")
+# print(" "*4, end="\n")
+# print(" "*4, end="")
+# print("/", end="")
+# print(" ", end="")
+# print("\\", end="")
+# print(" "*2, end="\n")
+# print(" "*2, end="")
+# print("[1]", end="")
+# print(" ", end="")
+# print("[3]", end="")
+# print(" "*2, end="\n")
+
+# s = "some"
+# space = 15
+# print(f"{s:>{space}}")
+# width = 40
+# print(f"|{'centered':^{width}}|")
+# width2 = 20
+# print(f"|{str(1):^{width2}}", end="")
+# print(f"{str(1):^{width2}}|")
+# width3 = 10
+# print(f"|{str(1):^{width3}}",end="")
+# print(f"{str(1):^{width3}}",end="")
+# print(f"{str(1):^{width3}}",end="")
+# print(f"{str(1):^{width3}}|",end="")
+
+
+# level = 8
+# print(level)
+# level /= 2
+# print(level)
+# level /= 2
+# print(level)
+
+if 0:
+    print("checl")
